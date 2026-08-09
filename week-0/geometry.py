bf90d6bc94b5ac6b592b1ad0ebb12d260e966503
Rishin Diwan <rishin18diwan@gmail.com>
2026-08-09 12:38:31 +0530

    Add geometry calculations for volume, area, and perimeter

---
+++ week-0/geometry.py
@@ -0,0 +1,11 @@
+length = int(input("What's the length? "))
+width = int(input("What's the width? "))
+height = int(input("What's the height? "))
+
+x = length * width * height
+y = length * width
+z = (length + width) * 2
+
+print(f"Volume: {x}")
+print(f"Area: {y}")
+print(f"Perimeter: {z}")
