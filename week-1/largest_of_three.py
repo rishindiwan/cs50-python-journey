558d54b3e769f8b06237562d7532bd434985d7cf
Rishin Diwan <rishin18diwan@gmail.com>
2026-09-06 10:05:23 +0530

    Add largest_of_three.py to find the greatest number

---
+++ week-1/largest_of_three.py
@@ -0,0 +1,12 @@
+a = int(input("Enter First number: "))
+b = int(input("Enter Second number: "))
+c = int(input("Enter Third number: "))
+
+if a >= b and a >= c:
+    print("a is greatest number")
+
+elif b >= a and b >= c:
+    print("b is greatest number")
+
+else:
+    print("c is greatest number")
